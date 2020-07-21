cb202ab06ca4bf12a334a15e3d7c5db89a5582d3
Stuart Thompson <stuart@stuartthompson.net>
2020-07-21 10:05:08 -0700

    Reference page uses markdown content component.

--- src/pages/ReferencePage.js
+++ src/pages/ReferencePage.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import MarkdownContent from '../components/MarkdownContent';
 
 /**
  * Renders a reference page.
- * 
- * @param content - The page content.
- * @param format - The source format. Default is "Markdown".
  */
-const ReferencePage = ({ content, format = "Markdown" }) => {
-    return (
-        <>
-            <div>Reference page</div>
-            <div>This page is ostensibly rendered from {content} in the {format} format.</div>
-        </>
-    );
+const ReferencePage = () => {
+  let { pageKey } = useParams();
+
+  // Read markdown content
+  const mdContent = `
+  # Markdown Test
+
+  This is a test markdown file used to test the MarkdownContent component.
+
+  ## Unit Testing
+
+  This will ultimately be converted into a unit test run via Jest.
+
+  Testing a hyperlink [Jest](https://jestjs.io/)
+  `;
+
+  return (
+    <>
+      <div>Reference page</div>
+      <div>Page key: {pageKey}</div>
+      <MarkdownContent content={mdContent} />
+    </>
+  );
 };
 
 export default ReferencePage;
