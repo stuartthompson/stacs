2a0c677e9d774113c1547084b232b0ea5f468678
Stuart Thompson <stuart@stuartthompson.net>
2020-07-21 10:04:57 -0700

    Formatting with prettier.

--- src/structure/PageRoutes.js
+++ src/structure/PageRoutes.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { LoginPage, ReferencePage } from "../pages";
+import { LoginPage, ReferencePage } from '../pages';
 
 /**
  * Renders pages based upon the current route.
  */
 const PageRoutes = () => {
-    return (
-        <Switch>
-            <Route path="/login">
-                <LoginPage />
-            </Route>
-            <Route path="/content/reference/:pagekey">
-                <ReferencePage />
-            </Route>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route path="/login">
+        <LoginPage />
+      </Route>
+      <Route path="/content/reference/:pageKey">
+        <ReferencePage />
+      </Route>
+    </Switch>
+  );
 };
 
 export default PageRoutes;
